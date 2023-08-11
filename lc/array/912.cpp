c9e08c6ed615f3b6eb1f4f943c75717078398551
Jacob-Chmura <jacobpaul.chmura@gmail.com>
2023-08-11 17:32:13 -0400

    add mergesort

--- lc/array/912.cpp
+++ lc/array/912.cpp
@@ -24,7 +24,30 @@ class Solution {
             return nums;
         }
 
+        vector<int> mergeSort(vector<int>& nums){
+            mergeSortHelper(nums, 0, nums.size() - 1);
+            return nums;
+        }
+
     private:
+        void mergeSortHelper(vector<int>& nums, int start, int end){
+            if(end-start+1 <= 1) return;
+            int middle = (start+end) / 2;
+            mergeSortHelper(nums, start, middle);
+            mergeSortHelper(nums, middle+1, end);
+            merge(nums, start, middle, end)
+        }
+
+        void merge(vector<int>& nums, int start, int middle, int end){
+            int l = start, r = middle+1, i = start;
+            while(l <= middle && r <= end){
+                if(nums[l] <= nums[r]) std::swap(nums[i++], nums[l++]);
+                else std::swap(nums[i++], nums[r++]);
+            }
+            while(l <= middle) std::swap(nums[i++], nums[l++]);
+            while(r <= end) std::swap(nums[i++], nums[r++]);
+        }
+
         void buildMaxHeap(vector<int>& nums, int n){
             for(int i = n/2 - 1; i >= 0; --i) heapify(nums, i, n);
         }
