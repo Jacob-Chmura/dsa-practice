c5982dfaf83d2ff0afaa958e3221970cc5058e2f
Jacob-Chmura <jacobpaul.chmura@gmail.com>
2023-08-13 17:52:42 -0400

    add more solutions

---
+++ lc/bin_search/374.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+  int guessNumber(int n) {
+    int l = 0, r = n;
+    while (l <= r) {
+      int m = l + (r - l) / 2;
+      int result = guess(m);
+      if (result == 1)
+        l = m + 1;
+      else if (result == -1)
+        r = m - 1;
+      else
+        return m;
+    }
+    return -1;
+  }
+};
